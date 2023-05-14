08de92e3b172f87538b11c833dd5159417c0cd93
Soheil G <119909404+sg3956@users.noreply.github.com>
2023-05-14 12:51:58 -0400

    Update buyer.py

--- buyer.py
+++ buyer.py
@@ -54,3 +54,177 @@ def app():
 # vehicles_df = vehicles_df[["id","price","year","model","odometer","cylinders","paint_color","type","state"]]
 # vehicles_df.to_sql('vehicles', conn, if_exists='append', index = False)
 
+
+import json
+from web3 import Web3
+
+# Replace with your Ethereum node URL
+eth_node_url = 'https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY'
+w3 = Web3(Web3.HTTPProvider(eth_node_url))
+
+# Replace with your smart contract ABI and address
+contract_abi = json.loads([
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_tokenAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_currencyTokenAddress",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_carId",
+				"type": "uint256"
+			}
+		],
+		"name": "buyCar",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "carCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "cars",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address payable",
+				"name": "seller",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "currencyToken",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getRemainingBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"name": "listCar",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract IERC721",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+])
+contract_address = 'CONTRACT_ADDRESS'
+
+# Replace with your private key and public address
+private_key = 'PRIVATE_KEY'
+my_address = 'PUBLIC_ADDRESS'
+
+# Set up the contract object
+contract = w3.eth.contract(address=Web3.toChecksumAddress(contract_address), abi=contract_abi)
+
+# Buy a car using the 'buyCar' function
+def buy_car(car_id):
+    car = contract.functions.cars(car_id).call()
+    car_price = car[2]
+
+    nonce = w3.eth.getTransactionCount(my_address)
+    gas_price = w3.eth.gasPrice
+
+    # Replace with the amount of Ether you're willing to pay for gas
+    gas_limit = 200000
+
+    transaction = contract.functions.buyCar(car_id).buildTransaction({
+        'from': my_address,
+        'gas': gas_limit,
+        'gasPrice': gas_price,
+        'nonce': nonce,
+    })
+
+    signed_txn = w3.eth.account.signTransaction(transaction, private_key)
+    txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+    return txn_hash.hex()
+
+# Get the remaining balance using the 'getRemainingBalance' function
+def get_remaining_balance():
+    remaining_balance = contract.functions.getRemainingBalance().call({'from': my
